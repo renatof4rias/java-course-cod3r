695562d33354c4d7be0e14c38706118a99a45f1c
renatof4rias <renatof4rias@gmail.com>
2024-01-22 12:22:05 -0300

    Aula 21 - Variaveis

---
+++ src/course_cod3r/modulo_01_/Variaveis.java
@@ -0,0 +1,37 @@
+package course_cod3r.modulo_01_;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+// Tipo Primitivo
+//		tipo     nome     valor      ;
+		int		   a   =     0       ;           
+	   double      b   =    0.0      ;
+	   char        c   =    'c'      ;
+	   String      d   =    "aaaa"   ;
+	   boolean     e   =    true     ; 
+	   boolean     f   =    false    ; 
+	                                      
+// Tipo Coringa 	                                    
+//		tipo     nome     valor      ;
+		var		 aa   =     0        ;           
+		var      bb   =    0.0       ;
+		var      cc   =    'c'       ;
+		var      dd   =    "aaaa"    ;
+		var      ee   =    true      ; 
+		var      ff   =    false     ; 	                                  
+	                                  
+	                                  
+// Atribuição
+		
+		double pi = 3.14;
+		double raio = 3.4;		
+		double area = pi * raio * raio;   
+		
+		
+		System.out.println("NOME DA VARIAVEL");
+		System.out.println(area);
+	}
+	
+}
