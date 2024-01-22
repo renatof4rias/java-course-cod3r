071072b9af34765c7afb97322598a2242ec65054
renatof4rias <renatof4rias@gmail.com>
2024-01-22 11:40:14 -0300

    Aula 20 - Comentarios

--- src/course_cod3r/modulo_01_/Comentarios.java
+++ src/course_cod3r/modulo_01_/Comentarios.java
@@ -1,15 +1,22 @@
 package course_cod3r.modulo_01_;
 
-import java.util.Iterator;
-
 public class Comentarios {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
-	type type = (type) iterator.next();
-	
-}
+
+		
+		//COMENTARIO DE LINHA
+		
+		/*
+		  	COMENTARIO
+ 					 DE
+ 						VARIAS
+							  LINHAS
+		 */
+		
+//		System.out.println("CONTROL + BARRA");
+		
+		
 	}
 
 }
