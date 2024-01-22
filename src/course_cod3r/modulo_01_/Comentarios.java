bf688b98580dc4bc13f026754f762bcd6b2c04c4
renatof4rias <renatof4rias@gmail.com>
2024-01-22 11:22:06 -0300

    teste

---
+++ src/course_cod3r/modulo_01_/Comentarios.java
@@ -0,0 +1,15 @@
+package course_cod3r.modulo_01_;
+
+import java.util.Iterator;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
+	type type = (type) iterator.next();
+	
+}
+	}
+
+}
